f0272c8f288457727d0ada33047d47aa0d126c2e
Jett <jett@legenddigital.cn>
2023-11-14 15:30:35 +0800

    1

---
+++ pkg/account/removal.go
@@ -0,0 +1,27 @@
+package account
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/luoxiandong/gosmart-sdk/pkg/common"
+	"github.com/luoxiandong/gosmart-sdk/pkg/store"
+	"github.com/mitchellh/go-homedir"
+)
+
+// RemoveAccount - removes an account from the keystore
+func RemoveAccount(name string) error {
+	accountExists := store.DoesNamedAccountExist(name)
+
+	if !accountExists {
+		return fmt.Errorf("account %s doesn't exist", name)
+	}
+
+	uDir, _ := homedir.Dir()
+	tronCTLDir := path.Join(uDir, common.DefaultConfigDirName, common.DefaultConfigAccountAliasesDirName)
+	accountDir := fmt.Sprintf("%s/%s", tronCTLDir, name)
+	os.RemoveAll(accountDir)
+
+	return nil
+}
